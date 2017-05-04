19c0959942b02b236ea6b5693155ad93e77851ab
Tamás Bunth <Tams Bunth>
2017-05-04 08:43:48 +0200

    impl CodedUITest methods
    Testing
        Add Button -> new entry not null
        preferences button -> pref title bar not null
        calendar button -> calendar not null

---
+++ JournaleyUITest/CodedUITest1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace JournaleyUITest
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestAddEntry_EntryNotNull()
+        {
+            this.UIMap.UIPreferenciesButton();
+            this.UIMap.AssertEntryNotNull();
+
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+        [TestMethod]
+        public void CodedUITestOpenPref_TitleBarNotNull()
+        {
+            this.UIMap.EntryAddedButton();
+            this.UIMap.AssertTitleBarNotNull();
+            this.UIMap.ClosePreferences();
+        }
+
+        [TestMethod]
+        public void CodedUITestCalendarButton_CalendarNotNull()
+        {
+
+            this.UIMap.CalendarButton();
+            this.UIMap.AssertCalendarNotNull();
+
+
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
